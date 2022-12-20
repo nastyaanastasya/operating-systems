5e3b267dae3bf7518cde3aff9dde403f1bd651f9
AnastasiaChukhina <nas2002@list.ru>
2022-12-21 01:11:14 +0300

    task-1 added

---
+++ task-1/parent.py
@@ -0,0 +1,28 @@
+#!#!/usr/bin/python3
+import os
+import random
+import sys
+
+
+def fork():
+    child = os.fork()
+    if child > 0:
+        print(f'Parent[{os.getpid()}]: I ran children process with PID {child}.')
+    else:
+        os.execl(sys.executable, sys.executable, "child.py", str(random.randint(5, 10)))
+
+
+n = int(sys.argv[1])
+for i in range(n):
+    fork()
+
+cnt = n
+while cnt > 0:
+    pid, status = os.wait()
+    if status != 0:
+        child = fork()
+    else:
+        print(f'Parent[{os.getpid()}]: Child with PID {pid} terminated. Exit Status {status}.')
+        cnt = cnt - 1
+
+os._exit(os.EX_OK)
